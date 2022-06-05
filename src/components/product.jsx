76cfc8dfeff11e1080300d3e8b3016238a577c21
Rikson Enrique Mauricio Palacios <Rikson.Mauricio@promart.pe>
2022-06-05 14:21:14 -0500

    mejora visión productos

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,7 +5,6 @@ import useGetProducts from '../hooks/useGetProducts'
 const Products = () => {
   const { query } = useAppContext()
   const { listProducts, loading, error } = useGetProducts()
-  console.log(listProducts)
 
   if (error) return <p>{Error}</p>
   if (loading) return <p>Loading</p>
@@ -13,15 +12,17 @@ const Products = () => {
   return (
     <section className='row py-5 gy-4'>
       {listProducts.map((index, key) => (
-        <div className='col-12 col-md-6 col-lg-3 col-sm-6' key={key}>
+        <div key={key} className='col-12 col-sm-6 col-md-6 col-lg-3 card' >
           <article className='card'>
-            <h1>Producto: {index.product_name}</h1>
-            <p>Precio: {index.price}</p>
-            <p>Categoría: {index.price}</p>
-            <img className='card-img-top' src={index.image} alt={index.product_name} />
-            <Link to={`/${index._id}`}>
-              <p>Detalle</p>
-            </Link>
+            <img className='card-img-top' src={index.image} alt={index.product_name} width='200' height='200'/>
+            <div className='card-body'>
+              <h1 className='card-title'>Producto: {index.product_name}</h1>
+              <p>Precio: {index.price}</p>
+              <p>Categoría: {index.price}</p>
+              <Link to={`/${index._id}`}>
+                <p>Detalle</p>
+              </Link>
+            </div>
           </article>
         </div>
       ))}
