48ce24dc92d7129641815a5d3800f297db2ce667
Rikson Enrique Mauricio Palacios <Rikson.Mauricio@promart.pe>
2022-06-07 01:51:42 -0500

    mejoras visuales

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -11,18 +11,18 @@ const Products = () => {
   if (loading) return <p>Loading</p>
 
   return (
-    <section className='row py-4 gy-3 prod-home'>
+    <section className='row gy-3 prod-home'>
       {listProducts.map((index, key) => (
-        <div key={key} className='col-12 col-sm-6 col-md-6 col-lg-3 card' >
-          <article className='card'>
-            <img className='card-img-top' src={index.image} alt={index.product_name} width='200' height='200'/>
+        <div key={key} className='col-lg-3 prod-cont' >
+          <article className='card prod'>
+            <img className='card-img-top img-prod' src={index.image} alt={index.product_name}/>
             <div className='card-body'>
-              <h1 className='card-title'>Producto: {index.product_name}</h1>
-              <p>Precio: {index.price}</p>
-              <p>Categoría: {index.price}</p>
-              <Link to={`/${index._id}`}>
-                <p>Detalle</p>
-              </Link>
+              <h1 className='card-title name-prod'>{index.product_name}</h1>
+              <div className='det-prod'>
+                <Link to={`/${index._id}`}>
+                  <p>S/. {index.price}</p>
+                </Link>
+              </div>
             </div>
           </article>
         </div>
